abd0ae3fda4af8eed66bd2e8af887f02d6035b07
mariazaikina <mashtakova93@gmail.com>
2023-04-13 11:12:07 +0400

    feat: hanoi

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,34 +6,34 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
 function dateSample(sampleActivity) {
-  let age;
-
-  if (sampleActivity < 0 || typeof sampleActivity != 'string') {
-      return false;
-  }     
-  else {
-      age = Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+  let age = 0;
+  if (sampleActivity < 0 || typeof sampleActivity != "string") {
+    return false;
+  } else if (typeof(+sampleActivity) !== 'number'||sampleActivity!= NaN){
+    return false;
+  }else {
+    age = Math.ceil(
+      Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD)
+    );
   }
-
-if (typeof age !== 'number' || age < 0){
-return false;
+  if (age < 0 || age === Infinity) {
+    return false;
+  }
+  return age;
 }
-return age;
-}
-
 
 module.exports = {
-  dateSample
+  dateSample,
 };
